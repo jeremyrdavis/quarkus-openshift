ab8ae360d531787457e815a0ee625a3d5229bd0a
Jeremy Davis <jeremyrdavis@mac.com>
2020-03-23 15:42:05 -0400

    added a version number

--- src/test/java/com/redhat/quarkus/examples/NativeyIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.quarkus.examples;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeyIT extends yTest {
-
-    // Execute the same tests but in native mode.
-}
