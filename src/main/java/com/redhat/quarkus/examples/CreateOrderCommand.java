f1fff79d57bc83c86215ab09f555345e76e6ec30
Jeremy Davis <jeremyrdavis@mac.com>
2020-03-23 16:31:43 -0400

    added methods to ApiResource

--- src/main/java/com/redhat/quarkus/examples/CreateOrderCommand.java
+++ src/main/java/com/redhat/quarkus/examples/CreateOrderCommand.java
@@ -1,5 +1,6 @@
 package com.redhat.quarkus.examples;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -35,4 +36,22 @@ public class CreateOrderCommand {
             this.kitchenOrders = Optional.ofNullable(kitchenOrdersList);
         }
     }
+
+    private List<Order> createBeverages() {
+
+        List<Order> beverages = new ArrayList(2);
+        beverages.add(new Order(Item.COFFEE_WITH_ROOM, "Mickey"));
+        beverages.add(new Order(Item.COFFEE_BLACK, "Minnie"));
+        return beverages;
+    }
+
+    private List<Order> createKitchenItems() {
+        List<Order> kitchenOrders = new ArrayList(2);
+        kitchenOrders.add(new Order(Item.CAKEPOP, "Mickey"));
+        kitchenOrders.add(new Order(Item.CROISSANT, "Minnie"));
+        return kitchenOrders;
+    }
+
+
+
 }
