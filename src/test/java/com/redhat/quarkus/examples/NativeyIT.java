a3a7e9b5593113f43ce4e3cb6fafcba6e805f5ac
Jeremy Davis <jeremyrdavis@mac.com>
2020-03-23 14:45:46 -0400

    initial

---
+++ src/test/java/com/redhat/quarkus/examples/NativeyIT.java
@@ -0,0 +1,9 @@
+package com.redhat.quarkus.examples;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeyIT extends yTest {
+
+    // Execute the same tests but in native mode.
+}
