2d1219afabfa134237360e26f89e5b24f041d7a2
Jeremy Davis <jeremyrdavis@mac.com>
2020-03-24 11:17:05 -0400

    added kafka input method

---
+++ src/main/java/com/redhat/quarkus/examples/KafkaProducer.java
@@ -0,0 +1,39 @@
+package com.redhat.quarkus.examples;
+
+import java.util.UUID;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
+import io.reactivex.Flowable;
+
+@ApplicationScoped
+public class KafkaProducer{
+
+    Jsonb jsonb = JsonbBuilder.create();
+
+    @Outgoing("orders-topic")
+    public Flowable<KafkaRecord<String, String>> generate() {
+
+        return Flowable.interval(500, TimeUnit.MILLISECONDS)    
+                .onBackpressureDrop()
+                .map(tick -> {
+                    final BeverageOrderInEvent event = new BeverageOrderInEvent(UUID.randomUUID().toString(), "Marcus", Item.ESPRESSO);
+                    System.out.println("Sending : " + jsonb.toJson(event).toString());
+                    return KafkaRecord.of(event.orderId, jsonb.toJson(event).toString());
+                });
+    }
+
+    @Incoming("orders-topic")
+    public void incoming(final KafkaRecord kafkaRecord){
+
+        System.out.println("receieved: " + kafkaRecord.getKey() + " " + kafkaRecord.getPayload());
+    }
+
+
+}
